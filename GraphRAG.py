0975dd6587f3d36ed768317df0a79cb92dc970b5
Revanth980727 <100169792+Revanth980727@users.noreply.github.com>
2025-07-05 21:26:45 -0400

    Update GraphRAG.py

--- GraphRAG.py
+++ GraphRAG.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from sqlalchemy import create_engine, MetaData, text
 from langchain.chat_models import ChatOpenAI
+import networkx
 
-import networkx as nx
-import networkx as nx
 # Replace with your actual MySQL credentials
 DATABASE_URL = "mysql+pymysql://USER:Password@localhost:3306/Database"
 engine = create_engine(DATABASE_URL)
